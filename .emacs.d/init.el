8c2847b7c02c5dcd4d1efa8320d141b3badcc68e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-03 16:23:06 +0900

    Emacs: Add configuration for El-Get package manager.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,16 @@
+;;; El-Get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(el-get 'sync)
+
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
