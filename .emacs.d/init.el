6e0849ab7258a6c9c2b6fcf9225378759d4a92d8
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-03 15:27:05 +0900

    Emacs: Add package repositories for ELPA.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 ;;; load-path
 ;(defun add-dir-and-subdirs-to-load-path (my-lisp-dir)
 ;  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
