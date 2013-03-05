12f2a50218fa34d8a97311f6e0af95737dcb93f7
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-05 22:22:37 +0900

    Emacs: Remove some specifications to 'default' face.

--- .emacs.d/darkfruit-theme.el
+++ .emacs.d/darkfruit-theme.el
@@ -49,6 +49,6 @@
  '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground "white" :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
  '(next-error ((t (:background "#5f0000" :foreground "#ff5555" :weight bold))))
  '(query-replace ((t (:inherit (isearch)))))
- '(default ((t (:stipple nil :background "#272727" :foreground "#FFFFFF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "Ricty")))))
+ '(default ((t (:stipple nil :background "#272727" :foreground "#FFFFFF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal)))))
 
 (provide-theme 'darkfruit)
