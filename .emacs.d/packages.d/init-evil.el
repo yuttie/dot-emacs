000bea7b7ad3453b8e0dc6b3c4c89277ebbcb159
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-05 14:08:40 +0900

    Emacs: Define key mappings for Evil to use inertial-scroll's commands.

--- .emacs.d/packages.d/init-evil.el
+++ .emacs.d/packages.d/init-evil.el
@@ -1,3 +1,7 @@
 ;;; Evil
 (require 'evil)
 (evil-mode 1)
+(eval-after-load 'inertial-scroll
+                 '(progn
+                    (define-key evil-normal-state-map (kbd "C-f") 'inertias-up)
+                    (define-key evil-normal-state-map (kbd "C-b") 'inertias-down)))
