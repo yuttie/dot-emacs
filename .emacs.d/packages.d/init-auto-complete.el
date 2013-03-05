c45d298b10e80504c071eeb91c5f831a6eaba524
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-05 14:18:53 +0900

    Emacs: Delete an unnecessary and wrong configuration for auto-complete.

--- .emacs.d/packages.d/init-auto-complete.el
+++ .emacs.d/packages.d/init-auto-complete.el
@@ -1,6 +1,5 @@
 ;;; Auto Complete Mode
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/site-lisp/auto-complete/dict")
 (ac-config-default)
 
 (define-key ac-completing-map (kbd "ESC")
