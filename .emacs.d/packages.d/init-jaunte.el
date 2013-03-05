1ad8cf5de440918686a256550f96220b865c5e9e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-05 14:13:00 +0900

    Emacs: Use 'autoload' instead of 'require'.

--- .emacs.d/packages.d/init-jaunte.el
+++ .emacs.d/packages.d/init-jaunte.el
@@ -1,4 +1,4 @@
 ;;; jaunte.el
 ;; http://kawaguchi.posterous.com/emacshit-a-hint
-(require 'jaunte)
+(autoload 'jaunte "jaunte")
 (global-set-key (kbd "C-c C-j") 'jaunte)
--- .emacs.d/packages.d/init-popwin.el
+++ .emacs.d/packages.d/init-popwin.el
@@ -1,4 +1,4 @@
 ;;; popwin.el
 ;;; https://github.com/m2ym/popwin-el
-(require 'popwin)
+(autoload 'popwin:display-buffer "popwin")
 (setq display-buffer-function 'popwin:display-buffer)
