0244a16211f3eec83c0ab8b4e88d3bdd63d55469
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-05 22:43:56 +0900

    Emacs: Adjust key mappings related to <C-h>.

--- .emacs.d/packages.d/init-elscreen.el
+++ .emacs.d/packages.d/init-elscreen.el
@@ -10,5 +10,3 @@
 (define-key elscreen-map (kbd "C-z") 'elscreen-toggle)
 (global-set-key (kbd "C-<tab>") 'elscreen-next)
 (global-set-key (kbd "C-S-<iso-lefttab>") 'elscreen-previous)
-(global-set-key (kbd "C-l") 'elscreen-next)
-(global-set-key (kbd "C-h") 'elscreen-previous)
--- .emacs.d/packages.d/init-evil.el
+++ .emacs.d/packages.d/init-evil.el
@@ -2,6 +2,8 @@
 (require 'evil)
 (evil-mode 1)
 (setq evil-default-cursor t)
+(define-key evil-insert-state-map (kbd "C-h") 'backward-delete-char-untabify)
+(define-key evil-ex-completion-map (kbd "C-h") 'evil-ex-delete-backward-char)
 (eval-after-load 'inertial-scroll
                  '(progn
                     (define-key evil-normal-state-map (kbd "C-f") 'inertias-up)
