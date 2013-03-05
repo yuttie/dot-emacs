386ca3c56dd032d00af202a9d197fc59f51a53a3
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-05 16:05:34 +0900

    Emacs: Make Evil use the default cursor color.

--- .emacs.d/packages.d/init-evil.el
+++ .emacs.d/packages.d/init-evil.el
@@ -1,6 +1,7 @@
 ;;; Evil
 (require 'evil)
 (evil-mode 1)
+(setq evil-default-cursor t)
 (eval-after-load 'inertial-scroll
                  '(progn
                     (define-key evil-normal-state-map (kbd "C-f") 'inertias-up)
