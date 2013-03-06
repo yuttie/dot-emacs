8a2583ed5743a0b9d81e768ded65b1ecaad96937
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-06 14:17:07 +0900

    Emacs: Don't include authorized tokens for twittering-mode in its configuration.

--- .emacs.d/packages.d/init-twittering-mode.el
+++ .emacs.d/packages.d/init-twittering-mode.el
@@ -1,14 +1,7 @@
 ;;; twittering-mode
 (autoload 'twit "twittering-mode" "Start twittering-mode." t)
 (autoload 'twittering-mode "twittering-mode" "Major mode for Twitter" t)
-(eval-after-load 'twittering-mode
-  '(progn
-     (setq twittering-account-authorization 'authorized)
-     (setq twittering-oauth-access-token-alist
-           '(("oauth_token" . "86241283-NT6lU5RKPsce0tM0GjJszev4kB8Jx5594Dz3A2y98")
-             ("oauth_token_secret" . "UAJWm1oLYWcQihn0PkTdJU1t90X2Ag1Suvml0gXBnU")
-             ("user_id" . "86241283")
-             ("screen_name" . "yuttieyuttie")))))
+(setq twittering-use-master-password t)
 (add-hook 'twittering-mode-hook
           (lambda ()
             (string-match "%s" twittering-status-format)
