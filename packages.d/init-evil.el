a54ba09da3c872047062f047d7d21bb6e55a79e4
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-09 11:52:29 +0900

    Add keys for moving around auto-complete's candidate list to insert state.

--- packages.d/init-evil.el
+++ packages.d/init-evil.el
@@ -4,6 +4,10 @@
 (setq evil-default-cursor t)
 (define-key evil-insert-state-map (kbd "C-h") 'backward-delete-char-untabify)
 (define-key evil-ex-completion-map (kbd "C-h") 'evil-ex-delete-backward-char)
+(eval-after-load 'auto-complete
+                 '(progn
+                    (define-key evil-insert-state-map (kbd "C-n") 'ac-next)
+                    (define-key evil-insert-state-map (kbd "C-p") 'ac-previous)))
 (eval-after-load 'inertial-scroll
                  '(progn
                     (define-key evil-normal-state-map (kbd "C-f") 'inertias-up)
