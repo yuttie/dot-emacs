0a1ba6770434b8299fafc4365fcc38a9ef7f99ce
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-09 22:25:03 +0900

    Bind 'magit-discard-item' to 'K'.

--- packages.d/init-magit.el
+++ packages.d/init-magit.el
@@ -1,4 +1,5 @@
 (add-hook 'magit-status-mode-hook
 	  (lambda ()
 	    (define-key magit-status-mode-map (kbd "j") 'next-line)
-	    (define-key magit-status-mode-map (kbd "k") 'previous-line)))
+	    (define-key magit-status-mode-map (kbd "k") 'previous-line)
+	    (define-key magit-status-mode-map (kbd "K") 'magit-discard-item)))
