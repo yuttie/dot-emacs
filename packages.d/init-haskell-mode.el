dc4c9d3c3595a74111e6d1fc342c03998121ff55
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-09 12:35:37 +0900

    Add an auto-complete source 'ghc-mod' in haskell-mode.

--- packages.d/init-haskell-mode.el
+++ packages.d/init-haskell-mode.el
@@ -5,4 +5,8 @@
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 (autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            (ghc-init)
+            (flymake-mode)
+            (add-to-list 'ac-sources ac-source-ghc-mod)))
