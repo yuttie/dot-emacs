8eb9000a0a3bdcfd886bae0b43a03e82307cf142
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-13 15:56:19 +0900

    Adjust an indent offset for Haskell.

--- packages.d/init-haskell-mode.el
+++ packages.d/init-haskell-mode.el
@@ -3,3 +3,5 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+(setq haskell-indentation-left-offset 4)
