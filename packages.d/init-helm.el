b4e9b2442c53bbca23316be8822ac958134bc3a0
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-27 00:14:57 +0900

    Change the key binding to invoke helm-mini to "C-c h".

--- packages.d/init-helm.el
+++ packages.d/init-helm.el
@@ -4,5 +4,5 @@
 (define-key helm-map (kbd "C-n") 'helm-next-line)
 (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
 (define-key helm-map (kbd "C-M-n") 'helm-next-source)
-(global-set-key (kbd "C-;") 'helm-mini)
+(global-set-key (kbd "C-c h") 'helm-mini)
 (helm-mode 1)
