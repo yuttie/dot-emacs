fae17d07402287e52e585edd523cb8fe512ed628
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-27 00:21:50 +0900

    Enable helm-mode (and load helm-mode.el) before configuration.

--- packages.d/init-helm.el
+++ packages.d/init-helm.el
@@ -1,8 +1,8 @@
 ;;; Helm
+(helm-mode 1)
 (setq helm-input-idle-delay 0)
 (define-key helm-map (kbd "C-p") 'helm-previous-line)
 (define-key helm-map (kbd "C-n") 'helm-next-line)
 (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
 (define-key helm-map (kbd "C-M-n") 'helm-next-source)
 (global-set-key (kbd "C-c h") 'helm-mini)
-(helm-mode 1)
