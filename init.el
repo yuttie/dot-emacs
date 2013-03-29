abab3dfb172307aa14cc6dddf7796af29b3c45e6
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-03-29 10:57:53 +0900

    Don't use the 'printing' package because it seems that only few people uses it.

--- init.el
+++ init.el
@@ -124,8 +124,6 @@
 ;;; printing
 ;; http://aki.issp.u-tokyo.ac.jp/itoh/PukiWiki/pukiwiki.php?Emacs
 (setq ps-multibyte-buffer 'non-latin-printer)
-(require 'printing)
-(pr-update-menus t)
 
 ;;; auto indentation
 (setq-default indent-tabs-mode nil)
