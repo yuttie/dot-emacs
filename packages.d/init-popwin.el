bd5b5edb56a1aaa60c98cf2ec0795625ad2d00da
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-04-02 16:40:52 +0900

    Update the basic configuration for 'popwin.el'.

--- packages.d/init-popwin.el
+++ packages.d/init-popwin.el
@@ -1,4 +1,4 @@
 ;;; popwin.el
 ;;; https://github.com/m2ym/popwin-el
-(autoload 'popwin:display-buffer "popwin")
-(setq display-buffer-function 'popwin:display-buffer)
+(require 'popwin)
+(popwin-mode 1)
