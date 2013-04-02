3c2e983649f0c527fd8f531c6fb6cb3b46e46fc2
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-04-03 00:08:11 +0900

    Use eval-after-load.

--- packages.d/init-ace-jump-mode.el
+++ packages.d/init-ace-jump-mode.el
@@ -1,2 +1,1 @@
 (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
--- packages.d/init-evil.el
+++ packages.d/init-evil.el
@@ -6,6 +6,9 @@
 (define-key evil-insert-state-map (kbd "C-h") (kbd "DEL"))
 (define-key evil-replace-state-map (kbd "C-h") (kbd "DEL"))
 (define-key evil-ex-completion-map (kbd "C-h") (kbd "DEL"))
+(eval-after-load 'ace-jump-mode
+                 '(progn
+                    (define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)))
 (eval-after-load 'auto-complete
                  '(progn
                     (define-key evil-insert-state-map (kbd "C-n") 'ac-next)
