d361d14818f0d1b57570fdf6bacbaa5c3b7ee8a5
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-04-02 16:41:37 +0900

    Add the configuration of popwin.el for helm and magit buffers.

--- packages.d/init-popwin.el
+++ packages.d/init-popwin.el
@@ -2,3 +2,6 @@
 ;;; https://github.com/m2ym/popwin-el
 (require 'popwin)
 (popwin-mode 1)
+
+(push '("^\\*helm.*\\*$" :regexp t) popwin:special-display-config)
+(push '("^\\*magit.*\\*$" :regexp t :height 0.5) popwin:special-display-config)
