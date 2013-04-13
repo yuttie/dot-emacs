371e76f9e114c5430bf23473e65bed913f652d53
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-04-13 14:52:51 +0900

    Update the names of arrow bitmaps.

--- packages.d/init-powerline.el
+++ packages.d/init-powerline.el
@@ -11,14 +11,14 @@
                               (powerline-raw (capitalize (symbol-name evil-state)) mode-line 'l)
 
                               (powerline-raw " "     mode-line)
-                              (powerline-arrow-right mode-line face1)
+                              (powerline-arrow-left mode-line face1)
 
                               (powerline-buffer-id   face1 'l)
                               (powerline-raw "%*"    face1 'l)
                               (powerline-buffer-size face1 'l)
 
                               (powerline-raw " "     face1)
-                              (powerline-arrow-right face1 face2)
+                              (powerline-arrow-left face1 face2)
 
                               (powerline-major-mode  face2 'l)
                               (powerline-process     face2 'l)
@@ -30,12 +30,12 @@
                               (powerline-raw global-mode-string                      face2 'r)
                               (powerline-raw (symbol-name buffer-file-coding-system) face2 'r)
 
-                              (powerline-arrow-left face2 face1)
+                              (powerline-arrow-right face2 face1)
                               (powerline-raw " "    face1)
 
                               (powerline-raw "%p" face1 'r)
 
-                              (powerline-arrow-left face1 mode-line)
+                              (powerline-arrow-right face1 mode-line)
                               (powerline-raw " "    mode-line)
 
                               (powerline-raw "%l" mode-line)
