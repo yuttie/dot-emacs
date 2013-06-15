e4fb7112eeafc05dc66b6bd597b4a3d51ec9e286
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-06-15 23:33:12 +0900

    Improve my own theme for Powerline so that it resembles that of Vim's Powerline.

--- packages.d/init-powerline.el
+++ packages.d/init-powerline.el
@@ -2,11 +2,29 @@
               '("%e"
                 (:eval
                  (let* ((active (powerline-selected-window-active))
-                        (mode-line (if active 'mode-line 'mode-line-inactive))
-                        (face1 (if active 'powerline-active1
-                                 'powerline-inactive1))
-                        (face2 (if active 'powerline-active2
-                                 'powerline-inactive2))
+                        (mode-face (cond
+                                    ((not active)                 'powerline-mode-inactive)
+                                    ((equal evil-state 'normal)   'powerline-mode-normal)
+                                    ((equal evil-state 'insert)   'powerline-mode-insert)
+                                    ((equal evil-state 'visual)   'powerline-mode-visual)
+                                    ((equal evil-state 'replace)  'powerline-mode-replace)
+                                    ((equal evil-state 'operator) 'powerline-mode-operator)))
+                        (face1 (cond
+                                ((not active)               'powerline-first-inactive)
+                                ((equal evil-state 'insert) 'powerline-first-insert)
+                                (t                          'powerline-first-normal)))
+                        (face2 (cond
+                                ((not active)               'powerline-second-inactive)
+                                ((equal evil-state 'insert) 'powerline-second-insert)
+                                (t                          'powerline-second-normal)))
+                        (face3 (cond
+                                ((not active)               'powerline-third-inactive)
+                                ((equal evil-state 'insert) 'powerline-third-insert)
+                                (t                          'powerline-third-normal)))
+                        (buffer-id-face (cond
+                                         ((not active)               'powerline-buffer-id-inactive)
+                                         ((equal evil-state 'insert) 'powerline-buffer-id-insert)
+                                         (t                          'powerline-buffer-id-normal)))
                         (separator-left
                          (intern (format "powerline-%s-%s"
                                          powerline-default-separator
@@ -16,41 +34,41 @@
                                          powerline-default-separator
                                          (cdr powerline-default-separator-dir))))
                         (lhs (list
-                              (powerline-raw (capitalize (symbol-name evil-state)) mode-line 'l)
+                              (powerline-raw (capitalize (symbol-name evil-state)) mode-face 'l)
 
-                              (powerline-raw " "     mode-line)
-                              (funcall separator-left mode-line face1)
+                              (powerline-raw " "     mode-face)
+                              (funcall separator-left mode-face face2)
 
-                              (powerline-buffer-id   face1 'l)
-                              (powerline-raw "%*"    face1 'l)
-                              (powerline-buffer-size face1 'l)
+                              (powerline-buffer-id   buffer-id-face 'l)
+                              (powerline-raw "%*"    face2 'l)
+                              (powerline-buffer-size face2 'l)
 
-                              (powerline-raw " "     face1)
-                              (funcall separator-left face1 face2)
+                              (powerline-raw " "     face2)
+                              (funcall separator-left face2 face3)
 
-                              (powerline-major-mode  face2 'l)
-                              (powerline-process     face2 'l)
-                              (powerline-minor-modes face2 'l)
-                              (powerline-narrow      face2 'l)
+                              (powerline-major-mode  face3 'l)
+                              (powerline-process     face3 'l)
+                              (powerline-minor-modes face3 'l)
+                              (powerline-narrow      face3 'l)
 
-                              (powerline-vc face2)))
+                              (powerline-vc face3)))
                         (rhs (list
-                              (powerline-raw global-mode-string                      face2 'r)
-                              (powerline-raw (symbol-name buffer-file-coding-system) face2 'r)
+                              (powerline-raw global-mode-string                      face3 'r)
+                              (powerline-raw (symbol-name buffer-file-coding-system) face3 'r)
+
+                              (funcall separator-right face3 face2)
+                              (powerline-raw " "    face2)
+
+                              (powerline-raw "%p" face2 'r)
 
                               (funcall separator-right face2 face1)
                               (powerline-raw " "    face1)
 
-                              (powerline-raw "%p" face1 'r)
-
-                              (funcall separator-right face1 mode-line)
-                              (powerline-raw " "    mode-line)
-
-                              (powerline-raw "%l" mode-line)
-                              (powerline-raw ":"  mode-line)
-                              (powerline-raw "%c" mode-line)
-                              (powerline-raw " "  mode-line 'r))))
+                              (powerline-raw "%l" face1)
+                              (powerline-raw ":"  face1)
+                              (powerline-raw "%c" face1)
+                              (powerline-raw " "  face1 'r))))
                    (concat
                     (powerline-render lhs)
-                    (powerline-fill face2 (powerline-width rhs))
+                    (powerline-fill face3 (powerline-width rhs))
                     (powerline-render rhs))))))
