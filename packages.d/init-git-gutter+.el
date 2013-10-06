79ed30a7a55abd97193a923282929d3c0c70a540
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-06 23:42:42 +0900

    Add key bindings for git-gutter+-mode

--- packages.d/init-git-gutter+.el
+++ packages.d/init-git-gutter+.el
@@ -1,1 +1,10 @@
 (global-git-gutter+-mode)
+
+(eval-after-load 'evil
+  '(progn
+     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-n") #'git-gutter+-next-hunk)
+     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-p") #'git-gutter+-previous-hunk)
+     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-v") #'git-gutter+-show-hunk)
+     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-r") #'git-gutter+-revert-hunks)
+     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-s") #'git-gutter+-stage-hunks)
+     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-c") #'git-gutter+-commit)))
