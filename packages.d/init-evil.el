5544665f779c352e5044abccff84f87b07df9b55
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-08 19:03:38 +0900

    Add functions and key bindings for folding using outline-minor-mode

--- packages.d/init-evil.el
+++ packages.d/init-evil.el
@@ -6,3 +6,51 @@
 (define-key evil-insert-state-map (kbd "C-h") (kbd "DEL"))
 (define-key evil-replace-state-map (kbd "C-h") (kbd "DEL"))
 (define-key evil-ex-completion-map (kbd "C-h") (kbd "DEL"))
+
+;;; fold
+(define-key evil-normal-state-map "zo" #'evil-open-fold)
+(define-key evil-normal-state-map "zO" #'evil-open-folds-at-point)
+(define-key evil-normal-state-map "zc" #'evil-close-fold)
+(define-key evil-normal-state-map "zC" #'evil-close-folds-at-point)
+(define-key evil-normal-state-map "za" nil)
+(define-key evil-normal-state-map "zA" nil)
+(define-key evil-normal-state-map "zm" #'evil-fold-more)
+(define-key evil-normal-state-map "zM" #'evil-close-all-folds)
+(define-key evil-normal-state-map "zr" #'evil-fold-less)
+(define-key evil-normal-state-map "zR" #'evil-open-all-folds)
+
+(evil-define-command evil-open-fold ()
+  "Open one fold under the cursor."
+  (outline-minor-mode)
+  (show-children))
+(evil-define-command evil-close-fold ()
+  "Close one fold under the cursor."
+  (outline-minor-mode)
+  (hide-subtree))
+(evil-define-command evil-open-folds-at-point ()
+  "Open all folds under the cursor recursively."
+  (outline-minor-mode)
+  (show-subtree))
+(evil-define-command evil-close-folds-at-point ()
+  "Close all folds under the cursor recursively."
+  (outline-minor-mode)
+  (hide-subtree))
+(evil-define-command evil-close-all-folds ()
+  "Close all folds."
+  (outline-minor-mode)
+  (hide-sublevels 1))
+(evil-define-command evil-open-all-folds ()
+  "Open all folds."
+  (outline-minor-mode)
+  (show-all))
+(evil-define-command evil-fold-more ()
+  "Fold more."
+  (outline-minor-mode)
+  (when (> evil-fold-level 0)
+    (decf evil-fold-level)
+    (hide-sublevels (+ evil-fold-level 1))))
+(evil-define-command evil-fold-less ()
+  "Reduce folding."
+  (outline-minor-mode)
+  (incf evil-fold-level)
+  (hide-sublevels (+ evil-fold-level 1)))
