adf269f36ad8ee45b9b9bb8f7b7cacf516d0792f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-11 19:51:31 +0900

    Use char-level ace-jump-mode instead word-level one

--- packages.d/init-ace-jump-mode.el
+++ packages.d/init-ace-jump-mode.el
@@ -1,5 +1,5 @@
-(global-set-key (kbd "C-c SPC") #'ace-jump-mode)
+(global-set-key (kbd "C-c SPC") #'ace-jump-char-mode)
 
 (eval-after-load 'evil
   '(progn
-     (define-key evil-motion-state-map (kbd "SPC") #'ace-jump-word-mode)))
+     (define-key evil-motion-state-map (kbd "SPC") #'ace-jump-char-mode)))
