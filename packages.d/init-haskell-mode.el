10983f38cfb270044daa881d2f0cf219d09186dc
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-27 22:30:40 +0900

    Use the haskell-interactive-mode instead of the inferior-haskell-mode

--- packages.d/init-haskell-mode.el
+++ packages.d/init-haskell-mode.el
@@ -5,6 +5,18 @@
 
 (setq haskell-indentation-left-offset 4)
 
+;;; use the haskell-interactive-mode instead of the inferior-haskell-mode
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-x C-d") nil)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+
 (eval-after-load 'evil
   '(progn
      (evil-define-key 'normal haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
