c93da4b28863db5cd1579b1fb0b076cb8e91f6df
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-27 16:41:18 +0900

    Remove unused commented-out lines

--- packages.d/init-haskell-mode.el
+++ packages.d/init-haskell-mode.el
@@ -1,7 +1,5 @@
 ;;; Haskell Mode
 (add-hook 'haskell-mode-hook #'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook #'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook #'turn-on-haskell-simple-indent)
 
 (setq haskell-indentation-left-offset 4)
