a8a8af373e0c8561f4d6ab939b24af9db06603c3
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-10-27 22:27:05 +0900

    Trivial change

--- packages.d/init-haskell-mode.el
+++ packages.d/init-haskell-mode.el
@@ -2,9 +2,10 @@
 (add-hook 'haskell-mode-hook #'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook #'turn-on-haskell-decl-scan)
+
+(setq haskell-indentation-left-offset 4)
+
 (eval-after-load 'evil
   '(progn
      (evil-define-key 'normal haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
      (evil-define-key 'normal haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
-
-(setq haskell-indentation-left-offset 4)
