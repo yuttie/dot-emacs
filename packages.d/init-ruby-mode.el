292bbf9696390229cb2b4e68783e1324033b55c0
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-11-03 15:07:29 +0900

    Don't automatically insert an encoding magic comment

--- packages.d/init-ruby-mode.el
+++ packages.d/init-ruby-mode.el
@@ -1,4 +1,5 @@
 (add-hook 'ruby-mode-hook
           (lambda ()
+            (setq ruby-insert-encoding-magic-comment nil)
             (local-set-key (kbd "RET") #'reindent-then-newline-and-indent)
             (local-set-key (kbd "C-j") #'reindent-then-newline-and-indent)))
