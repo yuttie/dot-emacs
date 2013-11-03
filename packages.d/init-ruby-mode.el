2df6cfa1df88d194ee4abe73e783cb41c2b8c31f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-11-03 15:18:37 +0900

    Use define-key instead of local-set-key

--- packages.d/init-ruby-mode.el
+++ packages.d/init-ruby-mode.el
@@ -2,8 +2,8 @@
           (lambda ()
             (setq ruby-insert-encoding-magic-comment nil)
             (setq ruby-deep-indent-paren nil)
-            (local-set-key (kbd "RET") #'reindent-then-newline-and-indent)
-            (local-set-key (kbd "C-j") #'reindent-then-newline-and-indent)))
+            (define-key ruby-mode-map (kbd "RET") #'reindent-then-newline-and-indent)
+            (define-key ruby-mode-map (kbd "C-j") #'reindent-then-newline-and-indent)))
 
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
