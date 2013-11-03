02a0a9701fec9c844327c43a79a9713f7926f7d6
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-11-03 15:08:46 +0900

    Open *.ru files, Gemfile and Rakefile in ruby-mode

--- packages.d/init-ruby-mode.el
+++ packages.d/init-ruby-mode.el
@@ -3,3 +3,7 @@
             (setq ruby-insert-encoding-magic-comment nil)
             (local-set-key (kbd "RET") #'reindent-then-newline-and-indent)
             (local-set-key (kbd "C-j") #'reindent-then-newline-and-indent)))
+
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
