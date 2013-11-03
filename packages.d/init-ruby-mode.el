3a54e1e7c53cdbae34fb04250d568bbaa94ff275
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-11-03 15:27:45 +0900

    Stop completion of end*

--- packages.d/init-ruby-mode.el
+++ packages.d/init-ruby-mode.el
@@ -3,7 +3,10 @@
             (setq ruby-insert-encoding-magic-comment nil)
             (setq ruby-deep-indent-paren nil)
             (define-key ruby-mode-map (kbd "RET") #'reindent-then-newline-and-indent)
-            (define-key ruby-mode-map (kbd "C-j") #'reindent-then-newline-and-indent)))
+            (define-key ruby-mode-map (kbd "C-j") #'reindent-then-newline-and-indent)
+            ;; auto-complete-mode
+            (make-local-variable 'ac-ignores)
+            (add-to-list 'ac-ignores "end")))
 
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
