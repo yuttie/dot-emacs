b8043de213110600db2ccc55acec5ef4eb7ae093
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-11-04 19:43:07 +0900

    Add the recipe for the ebuild-mode

--- init.el
+++ init.el
@@ -22,6 +22,13 @@
                      :type github
                      :pkgname "Emacs-D-Mode-Maintainers/Emacs-D-Mode"))
 (add-to-list 'el-get-sources
+             '(:name ebuild-mode
+                     :description "Edit Gentoo ebuild and eclass files"
+                     :website "http://wiki.gentoo.org/wiki/Project:Emacs"
+                     :type git
+                     :branch "ebuild-mode"
+                     :url "git://git.overlays.gentoo.org/proj/emacs-tools.git"))
+(add-to-list 'el-get-sources
              '(:name elscreen
                      :description "Screen Manager for Emacsen"
                      :type github
