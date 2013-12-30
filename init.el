2ab72c7308869c61c4a593dc1852a971a5a626eb
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-12-30 14:31:43 +0900

    Remove unused "xmlns"s

--- init.el
+++ init.el
@@ -41,9 +41,7 @@
                      (mapconcat #'identity
                                 `("<svg version=\"1.1\"
                                         baseProfile=\"full\"
-                                        xmlns=\"http://www.w3.org/2000/svg\"
-                                        xmlns:xlink=\"http://www.w3.org/1999/xlink\"
-                                        xmlns:ev=\"http://www.w3.org/2001/xml-events\">"
+                                        xmlns=\"http://www.w3.org/2000/svg\">"
                                   ,@(mapcar #'render-log logs)
                                   "</svg>")
                                 "\n")))))
