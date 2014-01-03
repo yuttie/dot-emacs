ea7ae0aa27dc3d213d2344b6d8b017887945c9ac
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-01-03 14:22:57 +0900

    Use "送り仮名の厳密なマッチ" instead of "送り仮名の優先的なマッチ"

--- init.el
+++ init.el
@@ -347,10 +347,21 @@ removed from them after the first call."
                      ("z(" nil "（")
                      ("z)" nil "）"))))
 
-     ;; 送り仮名の優先的なマッチ
-     (setq skk-henkan-strict-okuri-precedence t
-           skk-process-okuri-early nil
-           skk-henkan-okuri-strictly nil)
+     ;; 送り仮名の厳密なマッチ
+     (setq skk-henkan-okuri-strictly t)
+     (add-hook 'minibuffer-setup-hook
+               (lambda ()
+                 (when (and (boundp 'skk-henkan-okuri-strictly)
+                            skk-henkan-okuri-strictly
+                            (not (eq last-command 'skk-purge-jisyo)))
+                   (setq skk-henkan-okuri-strictly nil)
+                   (put 'skk-henkan-okuri-strictly 'temporary-nil t))))
+     (add-hook 'minibuffer-exit-hook
+               (lambda ()
+                 (when (and (get 'skk-henkan-okuri-strictly 'temporary-nil)
+                            (<= (minibuffer-depth) 1))
+                   (put 'skk-henkan-okuri-strictly 'temporary-nil nil)
+                   (setq skk-henkan-okuri-strictly t))))
 
      (global-set-key (kbd "C-x C-j") #'skk-mode)
      (global-set-key (kbd "C-x j") #'skk-auto-fill-mode)
