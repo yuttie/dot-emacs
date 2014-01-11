5c12de72bbb506ce8d6097d37a008eff46d5e2dc
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-01-11 14:52:53 +0900

    Autoload 'helm-descbinds'

--- packages.d/init-helm-descbinds.el
+++ packages.d/init-helm-descbinds.el
@@ -1,2 +1,5 @@
-(require 'helm-descbinds)
-(helm-descbinds-mode)
+(fmakunbound 'describe-bindings)
+(autoload 'describe-bindings "helm-descbinds"
+  "Show a list of all defined keys, and their definitions." t)
+(eval-after-load 'helm-descbinds
+  '(helm-descbinds-mode))
