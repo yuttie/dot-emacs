eb74debe28c8ed564e80507885a5f32821ebd329
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-02-11 17:38:20 +0900

    Use a precompiled header file when completing

--- packages.d/init-auto-complete-clang.el
+++ packages.d/init-auto-complete-clang.el
@@ -1,6 +1,11 @@
 (setq ac-clang-flags '("-std=c++11"))
 (make-variable-buffer-local 'ac-clang-flags)
 
+;; Precompiled header
+;; Use the following command line to generate it:
+;;   clang -x c++-header -std=c++11 ~/.precompile.h -o ~/.precompile.h.pch
+(setq ac-clang-prefix-header "~/.precompile.h.pch")
+
 (add-hook 'c-mode-common-hook
           (lambda ()
             (require 'auto-complete-clang)
