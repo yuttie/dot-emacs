1509c330ba9fb06770caf3d436dd945ae2fb6949
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-04-28 15:33:36 +0900

    Prevent ruby-mode-set-encoding from updating the magic comment

--- packages.d/init-ruby-mode.el
+++ packages.d/init-ruby-mode.el
@@ -1,6 +1,9 @@
 (add-hook 'ruby-mode-hook
           (lambda ()
             (setq ruby-insert-encoding-magic-comment nil)
+            (defadvice ruby-mode-set-encoding
+              (around disable-ruby-mode-set-encoding activate)
+              nil)
             (setq ruby-deep-indent-paren nil)
             (define-key ruby-mode-map (kbd "RET") #'reindent-then-newline-and-indent)
             (define-key ruby-mode-map (kbd "C-j") #'reindent-then-newline-and-indent)
