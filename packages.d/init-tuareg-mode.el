8b4458e35072cb54aca168977fda28c7dee81693
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-05-31 14:18:15 +0900

    Set evil-shift-width to 2 in tuareg-mode

--- packages.d/init-tuareg-mode.el
+++ packages.d/init-tuareg-mode.el
@@ -1,3 +1,6 @@
 (add-hook 'tuareg-mode-hook
           (lambda ()
-            (define-key tuareg-mode-map (kbd "RET") #'newline-and-indent)))
+            (define-key tuareg-mode-map (kbd "RET") #'newline-and-indent)
+            (eval-after-load 'evil
+              '(progn
+                 (setq evil-shift-width 2)))))
