576dffa3398b6c9a16316c04bf119cc53fd450f9
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-05-31 17:47:37 +0900

    Don't unindent lines starting with |

--- packages.d/init-tuareg-mode.el
+++ packages.d/init-tuareg-mode.el
@@ -1,5 +1,6 @@
 (add-hook 'tuareg-mode-hook
           (lambda ()
+            (setq tuareg-pipe-extra-unindent 0)
             (define-key tuareg-mode-map (kbd "RET") #'newline-and-indent)
             (eval-after-load 'evil
               '(progn
