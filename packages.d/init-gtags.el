7802e342640425b67384b9839b8245f34da9d068
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-07-01 17:38:17 +0900

    'C-c C-d' in gtags-mode automatically find a tag for a word at point

--- packages.d/init-gtags.el
+++ packages.d/init-gtags.el
@@ -1,6 +1,6 @@
 (eval-after-load 'gtags
   '(progn
-     (define-key gtags-mode-map (kbd "C-c C-d") #'gtags-find-tag)
+     (define-key gtags-mode-map (kbd "C-c C-d") #'gtags-find-tag-from-here)
      (define-key gtags-mode-map (kbd "C-c C-r") #'gtags-find-rtag)
      (define-key gtags-mode-map (kbd "C-c C-s") #'gtags-find-symbol)
      (define-key gtags-mode-map (kbd "C-c C-t") #'gtags-pop-stack)))
