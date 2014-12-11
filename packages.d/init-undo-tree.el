88ae26f58139bf684f2bbd2fd41cd28866bcc5c6
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-12-11 13:58:57 +0900

    Remove key-binding configurations for using undo-tree with evil
    Since these are provided by evil-integration.el.

--- packages.d/init-undo-tree.el
+++ packages.d/init-undo-tree.el
@@ -4,10 +4,3 @@
 (eval-after-load 'wl
   '(progn
      (add-hook 'wl-draft-mode-hook #'undo-tree-mode)))
-
-(eval-after-load 'evil
-  '(progn
-     (evil-define-key 'motion undo-tree-visualizer-map (kbd "j") #'undo-tree-visualize-redo)
-     (evil-define-key 'motion undo-tree-visualizer-map (kbd "k") #'undo-tree-visualize-undo)
-     (evil-define-key 'motion undo-tree-visualizer-map (kbd "h") #'undo-tree-visualize-switch-branch-left)
-     (evil-define-key 'motion undo-tree-visualizer-map (kbd "l") #'undo-tree-visualize-switch-branch-right)))
