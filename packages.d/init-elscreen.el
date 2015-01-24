c632d79445bde6726dbdddc8b6ee218b8e5690d8
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-01-24 21:48:05 +0900

    Bind "C-w T" to elscreen-split as with Vim

--- packages.d/init-elscreen.el
+++ packages.d/init-elscreen.el
@@ -10,3 +10,7 @@
 (define-key elscreen-map (kbd "C-s") #'elscreen-toggle)
 (global-set-key (kbd "C-<tab>") #'elscreen-next)
 (global-set-key (kbd "C-S-<iso-lefttab>") #'elscreen-previous)
+
+(eval-after-load 'evil
+  '(progn
+     (define-key evil-normal-state-map (kbd "C-w T") #'elscreen-split)))
