40c30613cc35a987124bb7342ce8846861d31c3a
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-01-28 18:29:50 +0900

    Make mmm-mode works with files containing PHP code

--- packages.d/init-mmm-mode.el
+++ packages.d/init-mmm-mode.el
@@ -2,5 +2,9 @@
 
 (setq mmm-global-mode 'maybe)
 
+(mmm-add-mode-ext-class 'html-mode nil 'html-css)
+(mmm-add-mode-ext-class 'html-mode nil 'html-js)
+(mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
 (mmm-add-mode-ext-class 'nxml-mode nil 'html-css)
 (mmm-add-mode-ext-class 'nxml-mode nil 'html-js)
+(mmm-add-mode-ext-class 'nxml-mode "\\.php\\'" 'html-php)
