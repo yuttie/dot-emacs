37cb0e84ef2da7770b59fdbe9f91ba67360cff16
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-05-24 17:21:12 +0900

    Customize company-mode

--- packages.d/init-company-mode.el
+++ packages.d/init-company-mode.el
@@ -1,24 +1,29 @@
 (add-hook 'after-init-hook #'global-company-mode)
 
+(setq company-tooltip-align-annotations t)
+(setq company-minimum-prefix-length 2)
+(setq company-require-match nil)
+(setq company-auto-complete 'company-explicit-action-p)
+(setq company-auto-complete-chars (lambda (input) t))
 (setq company-idle-delay 0)
-(setq company-frontends '(company-pseudo-tooltip-unless-just-one-frontend company-preview-frontend company-echo-metadata-frontend))
-(with-eval-after-load 'company
-  (setq
-   company-backends
-   '(company-elisp
-     company-bbdb
-     company-nxml
-     company-css
-     company-eclim
-     company-semantic
-     company-clang
-     company-xcode
-     company-cmake
-     (company-files company-dabbrev-code company-gtags company-etags company-keywords company-yasnippet)
-     company-oddmuse
-     company-dabbrev
-     company-capf
-     )))
+(setq company-frontends
+      '(company-pseudo-tooltip-unless-just-one-frontend
+        company-preview-frontend
+        company-echo-metadata-frontend))
+(setq company-backends
+      '(company-elisp
+        company-bbdb
+        company-nxml
+        company-css
+        company-eclim
+        company-semantic
+        company-clang
+        company-xcode
+        company-cmake
+        (company-files company-dabbrev-code company-gtags company-etags company-keywords company-yasnippet)
+        company-oddmuse
+        company-dabbrev
+        company-capf))
 
 (add-hook 'company-mode-hook
           (lambda ()
