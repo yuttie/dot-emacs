5a03ca17cd4088ced24b911e92a0f92083d5802d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-05-24 16:35:22 +0900

    Arrange the order of company-backends

--- packages.d/init-company-mode.el
+++ packages.d/init-company-mode.el
@@ -3,7 +3,22 @@
 (setq company-idle-delay 0)
 (setq company-frontends '(company-pseudo-tooltip-unless-just-one-frontend company-preview-frontend company-echo-metadata-frontend))
 (with-eval-after-load 'company
-  (add-to-list 'company-backends #'company-yasnippet))
+  (setq
+   company-backends
+   '(company-elisp
+     company-bbdb
+     company-nxml
+     company-css
+     company-eclim
+     company-semantic
+     company-clang
+     company-xcode
+     company-cmake
+     (company-files company-dabbrev-code company-gtags company-etags company-keywords company-yasnippet)
+     company-oddmuse
+     company-dabbrev
+     company-capf
+     )))
 
 (add-hook 'company-mode-hook
           (lambda ()
