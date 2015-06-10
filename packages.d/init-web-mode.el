8543512692fb133d557491b272e741f798ab9d8b
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-06-10 09:20:57 +0900

    Set web-mode-*-padding to 2

--- packages.d/init-web-mode.el
+++ packages.d/init-web-mode.el
@@ -1,5 +1,7 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 4)
 (setq web-mode-code-indent-offset 4)
+(setq web-mode-style-padding 2)
+(setq web-mode-script-padding 2)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
