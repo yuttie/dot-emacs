31deb2d8eef7fb65c083c6c7a00a95db1f24841c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-05 08:45:07 +0900

    Delay enabling git-gutter+-mode

--- packages.d/init-git-gutter+.el
+++ packages.d/init-git-gutter+.el
@@ -1,9 +1,11 @@
-(global-git-gutter+-mode)
+(add-hook 'find-file-hook #'git-gutter+-mode)
+(add-hook 'after-revert-hook #'git-gutter+-mode)
 
-(with-eval-after-load 'evil
-  (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-n") #'git-gutter+-next-hunk)
-  (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-p") #'git-gutter+-previous-hunk)
-  (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-v") #'git-gutter+-show-hunk)
-  (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-r") #'git-gutter+-revert-hunks)
-  (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-s") #'git-gutter+-stage-hunks)
-  (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-c") #'git-gutter+-commit))
+(with-eval-after-load 'git-gutter+
+  (with-eval-after-load 'evil
+    (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-n") #'git-gutter+-next-hunk)
+    (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-p") #'git-gutter+-previous-hunk)
+    (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-v") #'git-gutter+-show-hunk)
+    (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-r") #'git-gutter+-revert-hunks)
+    (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-s") #'git-gutter+-stage-hunks)
+    (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-c") #'git-gutter+-commit)))
--- packages.d/init-git-gutter-fringe+.el
+++ packages.d/init-git-gutter-fringe+.el
@@ -2,6 +2,7 @@
   (require 'git-gutter-fringe+)
   (remove-hook 'after-make-frame-functions #'load-git-gutter-fringe+))
 
-(if window-system
-    (load-git-gutter-fringe+)
-  (add-hook 'after-make-frame-functions #'load-git-gutter-fringe+))
+(with-eval-after-load 'git-gutter+
+  (if window-system
+      (load-git-gutter-fringe+)
+    (add-hook 'after-make-frame-functions #'load-git-gutter-fringe+)))
