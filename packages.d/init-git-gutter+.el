409a891894ebe5a2ab1bc5700664e25259554194
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-08 14:28:53 +0900

    Advise save-buffer to run git-gutter+-refresh first anyway

--- packages.d/init-git-gutter+.el
+++ packages.d/init-git-gutter+.el
@@ -2,6 +2,9 @@
 (add-hook 'after-revert-hook #'git-gutter+-mode)
 
 (with-eval-after-load 'git-gutter+
+  (defun git-gutter+-refresh-before-save-buffer (&rest args)
+    (git-gutter+-refresh))
+  (advice-add 'save-buffer :before #'git-gutter+-refresh-before-save-buffer)
   (with-eval-after-load 'evil
     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-n") #'git-gutter+-next-hunk)
     (evil-define-key 'normal git-gutter+-mode-map (kbd "C-d C-p") #'git-gutter+-previous-hunk)
