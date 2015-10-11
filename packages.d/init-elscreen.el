6bc5bb6fa8d2cf0d50705a0104711232c65e435a
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-11 12:48:33 +0900

    Improve the configuration of elscreen

--- packages.d/init-elscreen.el
+++ packages.d/init-elscreen.el
@@ -1,17 +1,17 @@
 ;;; ElScreen
-(require 'elscreen)
 (setq elscreen-prefix-key (kbd "C-s"))
 (setq elscreen-startup-command-line-processing nil)
-(setq elscreen-display-tab t)
 (setq elscreen-tab-display-control nil)
 (setq elscreen-tab-display-kill-screen nil)
-(elscreen-start)
-(define-key elscreen-map (kbd "s") #'elscreen-toggle)
-(define-key elscreen-map (kbd "C-s") #'elscreen-toggle)
-(global-set-key (kbd "C-<tab>") #'elscreen-next)
-(global-set-key (kbd "C-S-<iso-lefttab>") #'elscreen-previous)
-(global-set-key (kbd "<header-line> <mouse-4>") #'elscreen-previous)
-(global-set-key (kbd "<header-line> <mouse-5>") #'elscreen-next)
 
-(with-eval-after-load 'evil
-  (define-key evil-normal-state-map (kbd "C-w T") #'elscreen-split))
+(elscreen-start)
+
+(with-eval-after-load 'elscreen
+  (define-key elscreen-map (kbd "s")              #'elscreen-toggle)
+  (define-key elscreen-map (kbd "C-s")            #'elscreen-toggle)
+  (global-set-key (kbd "C-<tab>")                 #'elscreen-next)
+  (global-set-key (kbd "C-S-<iso-lefttab>")       #'elscreen-previous)
+  (global-set-key (kbd "<header-line> <mouse-4>") #'elscreen-previous)
+  (global-set-key (kbd "<header-line> <mouse-5>") #'elscreen-next)
+  (with-eval-after-load 'evil
+    (define-key evil-normal-state-map (kbd "C-w T") #'elscreen-split)))
