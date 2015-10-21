de0f691b40710507f6da47b4ed1432fd9520fc74
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-21 12:38:32 +0900

    Declare flycheck-{next,previous}-error commands as motions

--- packages.d/init-flycheck.el
+++ packages.d/init-flycheck.el
@@ -1,4 +1,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (with-eval-after-load 'flycheck
   (global-set-key (kbd "M-n") #'flycheck-next-error)
-  (global-set-key (kbd "M-p") #'flycheck-previous-error))
+  (global-set-key (kbd "M-p") #'flycheck-previous-error)
+  (with-eval-after-load 'evil
+    (evil-declare-motion 'flycheck-next-error)
+    (evil-declare-motion 'flycheck-previous-error)))
