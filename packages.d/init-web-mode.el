dd35697a3644050cbf94db4a9795da9b0182ae84
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-23 13:30:03 +0900

    Open more file types with web-mode

--- packages.d/init-web-mode.el
+++ packages.d/init-web-mode.el
@@ -4,4 +4,11 @@
 (setq web-mode-style-padding 2)
 (setq web-mode-script-padding 2)
 
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'"        . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'"     . web-mode))
