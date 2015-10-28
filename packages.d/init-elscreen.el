48a5462ea8c8838508fc27137b937e4a6978fc81
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-29 00:06:31 +0900

    Hide elscreen's tabs initially if navbar is installed

--- packages.d/init-elscreen.el
+++ packages.d/init-elscreen.el
@@ -4,6 +4,9 @@
 (setq elscreen-tab-display-control nil)
 (setq elscreen-tab-display-kill-screen nil)
 
+(when (el-get-package-installed-p 'navbar)
+  (setq elscreen-display-tab nil))
+
 (elscreen-start)
 
 (with-eval-after-load 'elscreen
