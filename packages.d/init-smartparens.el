ab8cac69ff109418b8beb90a0674c2252fef1a6b
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-11-04 12:34:14 +0900

    Enable smartparens-strict-mode only in lisp modes

--- packages.d/init-smartparens.el
+++ packages.d/init-smartparens.el
@@ -1,2 +1,7 @@
 (require 'smartparens-config)
-(smartparens-global-strict-mode)
+(smartparens-global-mode)
+
+(add-hook 'lisp-mode-hook             #'smartparens-strict-mode)
+(add-hook 'emacs-lisp-mode-hook       #'smartparens-strict-mode)
+(add-hook 'lisp-interaction-mode-hook #'smartparens-strict-mode)
+(add-hook 'ielm-mode-hook             #'smartparens-strict-mode)
