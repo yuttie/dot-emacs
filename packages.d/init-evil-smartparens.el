29b04ac33211cbcb38872d170926d445882077eb
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-11-04 12:33:39 +0900

    Enable evil-smartparens-mode only in lisp modes

--- packages.d/init-evil-smartparens.el
+++ packages.d/init-evil-smartparens.el
@@ -1,1 +1,4 @@
-(add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)
+(add-hook 'lisp-mode-hook             #'evil-smartparens-mode)
+(add-hook 'emacs-lisp-mode-hook       #'evil-smartparens-mode)
+(add-hook 'lisp-interaction-mode-hook #'evil-smartparens-mode)
+(add-hook 'ielm-mode-hook             #'evil-smartparens-mode)
