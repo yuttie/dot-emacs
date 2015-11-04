9d5b240adb0b6bc00610fbbfdd8184e14c6d3b8b
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-11-04 12:29:41 +0900

    Enable aggressive-indent-mode also in other lisp modes

--- packages.d/init-aggressive-indent-mode.el
+++ packages.d/init-aggressive-indent-mode.el
@@ -1,1 +1,4 @@
-(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+(add-hook 'lisp-mode-hook             #'aggressive-indent-mode)
+(add-hook 'emacs-lisp-mode-hook       #'aggressive-indent-mode)
+(add-hook 'lisp-interaction-mode-hook #'aggressive-indent-mode)
+(add-hook 'ielm-mode-hook             #'aggressive-indent-mode)
