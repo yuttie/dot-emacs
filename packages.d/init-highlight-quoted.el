84044f6177ce5b8218b022a0dfa6e6bca312d544
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-11-04 12:28:48 +0900

    Enable highlight-quoted-mode also in ielm-mode

--- packages.d/init-highlight-quoted.el
+++ packages.d/init-highlight-quoted.el
@@ -1,3 +1,4 @@
-(add-hook 'lisp-mode-hook #'highlight-quoted-mode)
-(add-hook 'emacs-lisp-mode-hook #'highlight-quoted-mode)
+(add-hook 'lisp-mode-hook             #'highlight-quoted-mode)
+(add-hook 'emacs-lisp-mode-hook       #'highlight-quoted-mode)
 (add-hook 'lisp-interaction-mode-hook #'highlight-quoted-mode)
+(add-hook 'ielm-mode-hook             #'highlight-quoted-mode)
