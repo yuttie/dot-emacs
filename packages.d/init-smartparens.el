dad83fc54b677ee20b804e437793266ef2bf9079
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-03-01 22:30:47 +0900

    Don't use smartparens-strict-mode

--- packages.d/init-smartparens.el
+++ packages.d/init-smartparens.el
@@ -1,7 +1,2 @@
 (require 'smartparens-config)
 (smartparens-global-mode)
-
-(add-hook 'lisp-mode-hook             #'smartparens-strict-mode)
-(add-hook 'emacs-lisp-mode-hook       #'smartparens-strict-mode)
-(add-hook 'lisp-interaction-mode-hook #'smartparens-strict-mode)
-(add-hook 'ielm-mode-hook             #'smartparens-strict-mode)
